0f40d052d62f0d7588b479881432baae69c7ae3e
zanlukakolaric <139894060+zanlukakolaric@users.noreply.github.com>
2023-08-23 14:46:48 +0200

    Add files via upload

---
+++ funkcije.py
@@ -0,0 +1,139 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+#dobis podatke za kriptovlute. Pazit moraš, da imena pišeš kot "BTC-USD"
+
+def pridobi_podatke(imena_kriptovalut, z ="2015-09-17", k="2023-08-01"):
+    podatki = {}
+    for ime in imena_kriptovalut:
+        podatki_o_kriptovaluti = yf.download(ime, z, k)
+        podatki[ime] = podatki_o_kriptovaluti
+    
+    
+    return podatki
+
+podatki_o_kriptovalutah = pridobi_podatke(["BTC-USD"])
+def dodaj_dnevna_rast(podatki_o_kriptovalutah):
+    for kriptovaluta, podatki in podatki_o_kriptovalutah.items():
+        podatki["Dnevna rast"] = (podatki["Close"] - podatki["Open"]).round(4)
+        podatki_o_kriptovalutah[kriptovaluta]  = podatki
+    return podatki_o_kriptovalutah
+    
+    
+    
+
+    
+    
+    
+    
+    
+def povprecje(podatki_o_kriptovalutah):
+    for kriptuvaluta, podatki in podatki_o_kriptovalutah.items():
+        podatki["Povprečje"] = ((podatki["Close"] + podatki["Open"]) /2).round(4)
+        podatki_o_kriptovalutah[kriptuvaluta] = podatki
+    return podatki_o_kriptovalutah
+
+    
+    
+    
+    
+    
+    
+def rast_v_procentih(podatki_o_kriptovalutah):
+    for kriptuvaluta, podatki in podatki_o_kriptovalutah.items():
+        podatki["Rast v %"] = (((podatki["Close"]/podatki["Open"]) - 1)*100).round(4)
+        podatki_o_kriptovalutah[kriptuvaluta] = podatki
+    return podatki_o_kriptovalutah
+
+def rast_glede_na_x(podatki_o_kriptovalutah, x = None):
+    for kriptuvaluta, podatki in podatki_o_kriptovalutah.items():
+        if x is None:
+            return podatki_o_kriptovalutah
+        else:
+            return podatki_o_kriptovalutah  
+    
+    
+    
+    
+    
+def uredi_podatke(podatki_o_kriptovalutah):
+    for kriptuvaluta, podatki in podatki_o_kriptovalutah.items():
+        podatki["Close"] = podatki["Close"].round(4)
+        podatki["Open"] = podatki["Open"].round(4)
+        podatki["High"] = podatki["High"].round(4)
+        podatki["Low"] = podatki["Low"].round(4)
+        podatki["Adj Close"] = podatki["Adj Close"].round(4)
+        podatki["Volume"] = podatki["Volume"].round(4)
+        podatki_o_kriptovalutah[kriptuvaluta] = podatki.ffill()   #to dopolni prazna mesta z zadnjimi znanimi vrednostmi
+    return podatki_o_kriptovalutah
+
+
+
+def shrani(podatki_o_kriptovalutah):
+    os.makedirs("datoteke", exist_ok=True)
+    for ime, podatki in podatki_o_kriptovalutah.items():
+        file_name = f"{ime}.csv"
+        podatki.to_csv("datoteke/" + file_name)
+    
+    
+def graf_povprecnih_cen(podatki_o_kriptovalutah):
+    plt.figure(figsize=(12, 6))
+    for kriptovaluta, podatki in podatki_o_kriptovalutah.items(): 
+        podatki["Povprečje"].plot(label=f"{kriptovaluta}")
+    
+    plt.title("Gibanje povprečnih cen")
+    plt.xlabel("Datum")
+    plt.ylabel("Cena v USD")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+def graf_odpiralnih_cen(podatki_o_kriptovalutah):
+    plt.figure(figsize=(12, 6))
+    for kriptovaluta, podatki in podatki_o_kriptovalutah.items(): 
+        podatki["Open"].plot(label=f"{kriptovaluta}")
+    
+    plt.title("Gibanje odpiralnih cen")
+    plt.xlabel("Datum")
+    plt.ylabel("Cena v USD")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
+
+
+
+def graf_rasti_v_procentih(podatki_o_kriptovalutah):
+    plt.figure(figsize=(12, 6))
+    for kriptovaluta, podatki in podatki_o_kriptovalutah.items(): 
+        podatki["Rast v %"].plot(label=f"{kriptovaluta}")
+    
+    plt.title("Rast kriptovalut v %")
+    plt.xlabel("Datum")
+    plt.ylabel("Rast v %")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    
+    
+def graf_volumna(podatki_o_kriptovalutah):
+    plt.figure(figsize=(12, 6))
+    for kriptovaluta, podatki in podatki_o_kriptovalutah.items(): 
+        podatki["Volume"].plot(label=f"{kriptovaluta}")
+    
+    plt.title("Volumen")
+    plt.xlabel("Datum")
+    plt.ylabel("Volumen")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    
+
+
+
+print(graf_volumna((uredi_podatke(dodaj_dnevna_rast(povprecje(rast_v_procentih(podatki_o_kriptovalutah)))))))
+
+
